3b2f5b9ea41f0b5c4c45a3b07466585317fe76fe
mjk-hudson <majikhudson@gmail.com>
2026-03-09 18:05:03 -0400

    header bar and technologies updates that include additional technologies and resized header bar

--- src/routes/HeaderBar.jsx
+++ src/routes/HeaderBar.jsx
@@ -42,9 +42,9 @@ export default function HeaderBar()
 
           {/*Logo section */}
           <motion.img
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            transition={{duration: 0.5, delay: 0.2}} 
+            initial={{opacity: 0, scale:0}}
+            animate={{opacity: 1, scale: 1}}
+            transition={{duration: 0.5, delay: 0.5, type: 'spring', stiffness:400, damping: 20}} 
             className='w-24 h-auto rounded-xl'
             src={Majik}
             alt="Profile Image"/>
--- src/routes/Technologies.jsx
+++ src/routes/Technologies.jsx
@@ -10,6 +10,8 @@ import{SiTailwindcss} from 'react-icons/si';
 import{TbBrandFramerMotion} from 'react-icons/tb';
 import {PiFigmaLogoDuotone} from 'react-icons/pi';
 import { SiWebpack } from "react-icons/si";
+import { SiBootstrap } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
 import {motion} from 'motion/react';
 
 const iconVariants = (duration) => ({
@@ -39,78 +41,145 @@ export default function Technologies() {
             variants={iconVariants(2.5)}
             initial= "initial"
             animate= "animate"
-            className="rounded-2xl border-2 border-purple-900 p-4">
+            whileHover={{scale: 1.2}}
+            className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <PiFigmaLogoDuotone className="text-2xl text-pink-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-pink-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Figma
+            </span>
         </motion.div>
-        <motion.div 
+        <motion.div
             variants={iconVariants(3)}
             initial='initial'
+            whileHover={{scale: 1.2}}
             animate= 'animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+            className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <DiHtml5 className="text-2xl text-red-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                HTML5
+            </span>
+
         </motion.div>
         <motion.div 
-        variants={iconVariants(3.5)}
-        initial='initial'
-        animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+          whileHover={{scale: 1.2}}
+          variants={iconVariants(3.5)}
+          initial='initial'
+          animate='animate'
+          className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <DiCss3Full className="text-2xl text-orange-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-orange-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              CSS3
+            </span>
         </motion.div>
-        <motion.div 
-        variants={iconVariants(4)}
-        initial='initial'
-        animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        <motion.div
+          whileHover={{scale: 1.2}} 
+          variants={iconVariants(4)}
+          initial='initial'
+          animate='animate'
+          className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <DiJsBadge className="text-2xl text-yellow-400" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-yellow-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                JavaScript
+            </span>
         </motion.div>
-        <motion.div 
+        <motion.div
+        whileHover={{scale: 1.2}} 
         variants={iconVariants(4.5)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
-            <RiReactjsLine className="text-2xl text-cyan-400" />
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
+          <RiReactjsLine className="text-2xl text-cyan-400" />
+          {/* icon Tooltip */}
+          <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-cyan-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            React.js
+          </span>
         </motion.div>
-        <motion.div 
-        variants={iconVariants(3)}
+        <motion.div
+        whileHover={{scale: 1.2}} 
+        variants={iconVariants(4.5)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
+          <SiTypescript className="text-2xl text-blue-700" />
+          {/* icon Tooltip */}
+          <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-blue-700 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            TypeScript
+          </span>
+        </motion.div>
+        <motion.div
+          whileHover={{scale: 1.2}} 
+          variants={iconVariants(3)}
+          initial='initial'
+          animate='animate'
+          className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <DiGit className="text-2xl text-neutral-400" />
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-neutral-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Git
+            </span>
         </motion.div>
-        <motion.div 
-        variants={iconVariants(3.5)}
-        initial='initial'
-        animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
-            <SiMongodb  className="text-2xl text-green-500" />
+        <motion.div
+          whileHover={{scale: 1.2}} 
+          variants={iconVariants(3.5)}
+          initial='initial'
+          animate='animate'
+          className="group relative rounded-2xl border-2 border-purple-900 p-4">
+              <SiMongodb  className="text-2xl text-green-500" />
+              {/* icon Tooltip */}
+              <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-green-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Mongo DB</span>
         </motion.div>
-        <motion.div 
+        <motion.div
+        whileHover={{scale: 1.2}} 
         variants={iconVariants(2.5)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <SiTailwindcss className="text-2xl text-indigo-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-indigo-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Tailwind CSS</span>
         </motion.div>
-        <motion.div 
+        <motion.div
+        whileHover={{scale: 1.2}} 
+        variants={iconVariants(2.5)}
+        initial='initial'
+        animate='animate'
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
+            <SiBootstrap className="text-2xl text-blue-700" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-blue-700 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Bootstrap CSS</span>
+        </motion.div>
+        <motion.div
+        whileHover={{scale: 1.2}} 
         variants={iconVariants(2)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <FaNodeJs className="text-2xl text-green-400" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-green-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Node.js</span>
         </motion.div>
-        <motion.div 
+        <motion.div
+        whileHover={{scale: 1.2}} 
         variants={iconVariants(3.5)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <TbBrandFramerMotion className="text-2xl text-purple-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-purple-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Framer- Motion.dev</span>
         </motion.div>
-        <motion.div 
+        <motion.div
+        whileHover={{scale: 1.2}} 
         variants={iconVariants(2.5)}
         initial='initial'
         animate='animate'
-        className="rounded-2xl border-2 border-purple-900 p-4">
+        className="group relative rounded-2xl border-2 border-purple-900 p-4">
             <SiWebpack className="text-2xl text-blue-500" />
+            {/* icon Tooltip */}
+            <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-slate-800 px-2 py-1 text-[10px] text-blue-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100">Webpack</span>
         </motion.div>
       </motion.div>
     </div>
