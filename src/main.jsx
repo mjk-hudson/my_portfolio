6825080b262098ae14685b7abc89b0ccef4dfc4a
mjk-hudson <majikhudson@gmail.com>
2026-05-13 15:31:40 -0400

    updated the base path import

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import About from './routes/About.jsx'
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: import.meta.env.BASE_URL,
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
