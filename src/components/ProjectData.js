062169195250f1481009f5861ca01b9fb0d6985d
mjk-hudson <majikhudson@gmail.com>
2026-06-17 14:12:41 -0400

    image update

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,7 +2,7 @@ import BledgerHero from '../assets/Bledger-board.png';
 import BledgerSketches from '../assets/bledger/Bledger-Sketchbook.png';
 import BledgerAudit from '../assets/bledger/Bledger-competitive-audit.png';
 import BledgerEmpathy from '../assets/bledger/Bledger-empathy-map.png';
-import BledgerHome from '../assets/bledger/Bledger-home.png';
+import BledgerHome from '../assets/bledger/Bledger-Home.png';
 import BledgerHomeDark from '../assets/bledger/Bledger-home-dark.png';
 import BLedgerWireframe1 from '../assets/bledger/bledger-wireframe1.png';
 import BledgerWireframe2 from '../assets/bledger/bledger-wireframe2.png';
